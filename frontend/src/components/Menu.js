0acd269eab02f09653d4c18618cfd9114b0793bf
Rosta Gábor <rostagabor13@gmail.com>
2024-03-06 14:46:25 +0000

    React: Extended Account page, Menu checks user's session

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,25 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { Link, useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
 import { authObserver } from "../utils/auth";
 
 export default function Menu() {
   const { i18n, t } = useTranslation();
 
   const [token, setToken] = useState(sessionStorage.getItem("token"));
+  const [sessionExpired, setSessionExpired] = useState(false);
+
+  const timeoutId = useRef(null);
 
   useEffect(() => {
+    let privateToken = sessionStorage.getItem("token");
+
     authObserver.subscribe("token", data => {
+      privateToken = data;
       sessionStorage.setItem("token", data);
       setToken(data);
-    });
-    return () => { authObserver.unsubscribe("token"); }
-  }, []);
 
+      //Clear the existing timeout
+      if (timeoutId) {
+        clearTimeout(timeoutId.current);
+      }
+
+      //Start a new timeout
+      if (privateToken) {
+        timeoutId.current = setTimeout(() => {
+          axios.get(`${BASE_URL}/users/check?token=${privateToken}`)
+          .catch(_ => {
+            privateToken = "";
+            setToken("");
+            authObserver.publish("token", "");
+            setSessionExpired(true);
+          });
+        }, 65 * 60 * 1000); //65 minutes (token expires in 1 hour)
+      }
+    });
+
+    return () => {
+      authObserver.unsubscribe("token");
+
+      if (timeoutId) {
+        clearTimeout(timeoutId.current);
+      }
+    };
+  }, []);
 
   const navigate = useNavigate();
 
@@ -30,38 +63,45 @@ export default function Menu() {
   };
 
   return (
-    <Navbar expand="lg" className="yellow">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          <img src="/logo.png" alt={t("logo")} width="30" height="30" className="d-inline-block align-top" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/help">{t("help-title")}</Nav.Link>
-            <Nav.Link as={Link} to="/generate-maze">{t("maze-generate-title")}</Nav.Link>
-          </Nav>
-          <Nav className="ms-auto">
-            {token ?
-              <>
-                <Nav.Link as={Link} to="/account">{t("account-title")}</Nav.Link>
-                <Nav.Link onClick={logout}>{t("logout")}</Nav.Link>
-              </>
-            :
-              <>
-                <Nav.Link as={Link} to="/signup">{t("signup-title")}</Nav.Link>
-                <Nav.Link as={Link} to="/login">{t("login-title")}</Nav.Link>
-              </>
-            }
-            <Form variant="outlined" size="small">
-              <Form.Select aria-label={t("language-change")} onChange={e => i18n.changeLanguage(e.target.value)} value={i18n.resolvedLanguage} >
-                <option value="en">🇬🇧</option>
-                <option value="hu">🇭🇺</option>
-              </Form.Select>
-            </Form>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="yellow">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img src="/logo.png" alt={t("logo")} width="30" height="30" className="d-inline-block align-top" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/help">{t("help-title")}</Nav.Link>
+              <Nav.Link as={Link} to="/generate-maze">{t("maze-generate-title")}</Nav.Link>
+            </Nav>
+            <Nav className="ms-auto">
+              {token ?
+                <>
+                  <Nav.Link as={Link} to="/account">{t("account-title")}</Nav.Link>
+                  <Nav.Link onClick={logout}>{t("logout")}</Nav.Link>
+                </>
+              :
+                <>
+                  <Nav.Link as={Link} to="/signup">{t("signup-title")}</Nav.Link>
+                  <Nav.Link as={Link} to="/login">{t("login-title")}</Nav.Link>
+                </>
+              }
+              <Form variant="outlined" size="small">
+                <Form.Select aria-label={t("language-change")} onChange={e => i18n.changeLanguage(e.target.value)} value={i18n.resolvedLanguage} >
+                  <option value="en">🇬🇧</option>
+                  <option value="hu">🇭🇺</option>
+                </Form.Select>
+              </Form>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Modal show={sessionExpired} onHide={() => setSessionExpired(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>{t("session-expired")}</Modal.Title>
+        </Modal.Header>
+      </Modal>
+    </>
   );
 }
--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,23 +1,95 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { Navigate } from "react-router-dom";
+import { Spinner, Row, Col, Alert, Button, Card } from "react-bootstrap";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { authObserver } from "../utils/auth";
 
 export default function Account() {
-  const { t } = useTranslation();
+  const { i18n, t } = useTranslation();
 
-  useEffect(() => { document.title = t("account-title") + " | " + t("app-name"); });
+  const operations = ["operation-addition", "operation-subtraction", "operation-addition-subtraction", "operation-multiplication", "operation-division", "operation-multiplication-division"];
 
-  const token = sessionStorage.getItem("token");
+  useEffect(() => { 
+    document.title = t("account-title") + " | " + t("app-name"); 
+  });
 
-  if (!token) {
-    return <Navigate to={"/login/?next=/account"} replace />;
-  }
+  const [token, setToken] = useState(sessionStorage.getItem("token"));
+
+  const [loading, setLoading] = useState(true);
+  const [mazes, setMazes] = useState(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    let privateToken = sessionStorage.getItem("token");
+
+    axios.get(`${BASE_URL}/maze/getAll?token=${privateToken}`)
+    .then(response => {
+      privateToken = response.data.token;
+      setToken(privateToken);
+      authObserver.publish("token", privateToken);
+
+      setTimeout(() => {
+        setLoading(false);
+        setMazes(response.data.mazes);
+      }, 1000);
+    })
+    .catch(_ => {
+      setError("error-unknown");
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <>
       <center>
         <h1>{t("account-title")}</h1>
       </center>
+
+      {error && <Alert variant="danger">{t(error)}</Alert>}
+
+      {loading && 
+        <>
+          <Spinner animation="border" />
+          <p>{t("loading")}</p>
+        </>
+      }
+
+      {mazes && mazes.length === 0 && <Alert variant="info">{t("no-mazes")}</Alert>}
+
+      {mazes && mazes.length > 0 && 
+        <Col xs={12} md={8} className="mx-auto">
+          {mazes.map((maze, index) => (
+            <Card key={index} className="mb-3">
+              <Card.Body>
+                <Card.Title>{t("maze-title")} #{maze.id}</Card.Title>
+                <div>
+                  <p>{t("maze-description")}: {maze.description ? maze.description : "-"}</p>
+                  <Row>
+                    <Col>
+                      <p>{t("maze-size", { height: maze.height, width: maze.width })}</p>
+                      <p>{t("maze-operation-type")}: {t(operations[maze.operation])}</p>
+                      <p>{t("maze-generate-range")}: {maze.numbersRangeStart}-{maze.numbersRangeEnd}</p>
+                    </Col>
+                    <Col>
+                      <p>{t("maze-length-of-the-path", { length: maze.pathLength })}</p>
+                      <p>{t("maze-generate-path-type")}: {maze.pathTypeEven ? t("maze-generate-path-type-even") : t("maze-generate-path-type-odd")}</p>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Button variant="primary">{t("maze-edit-details")}</Button>
+                    </Col>
+                    <Col>
+                      <p className="text-end">{t("maze-created-at", { date: new Date(maze.createdAt).toLocaleString(i18n.resolvedLanguage) })}</p>
+                    </Col>
+                  </Row>
+                </div>
+              </Card.Body>
+            </Card>
+          ))}
+        </Col>
+      }
     </>
   );
 }
