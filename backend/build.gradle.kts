85a3dd88b6f04120ced4658cf7278b9b51cc8b85
Rosta Gábor <rostagabor13@gmail.com>
2024-03-28 08:29:14 +0000

    Kotlin: Small changes

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,17 +2,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-	id("org.springframework.boot") version "3.2.2"
+	id("org.springframework.boot") version "3.2.4"
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.22"
-	kotlin("plugin.spring") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "com.rostagabor"
-version = "0.0.1-SNAPSHOT"
+version = "1.0"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -24,10 +25,10 @@ repositories {
 
 dependencies {
 	//Spring Boot & Kotlin
-	implementation("org.springframework.boot:spring-boot-starter-web:3.2.3")
-	implementation("org.springframework.boot:spring-boot-starter-security:3.2.3")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
-	implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.2.4")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.2.4")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.23")
 
 	//ML
 	implementation("org.jetbrains.kotlinx:kotlin-deeplearning-tensorflow:0.5.2")
@@ -38,11 +39,11 @@ dependencies {
 
     //Database
 	implementation("org.hsqldb:hsqldb:2.7.2")
-	implementation("org.postgresql:postgresql:42.7.2")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.3")
+	implementation("org.postgresql:postgresql:42.7.3")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
 
 	//Mail
-	implementation("org.springframework.boot:spring-boot-starter-mail:3.2.3")
+	implementation("org.springframework.boot:spring-boot-starter-mail:3.2.4")
 
 	//JWT
 	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
@@ -50,7 +51,7 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
 	//Tests
-	testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.3")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.4")
 }
 
 tasks.withType<KotlinCompile> {
