a26906932e7a8f58625665d79b28ad504b4cdc76
Rosta Gábor <rostagabor13@gmail.com>
2024-03-28 08:34:47 +0000

    Kotlin: Small changes

--- backend/src/test/kotlin/com/rostagabor/mathmaze/MathMazeApplicationTests.kt
+++ backend/src/test/kotlin/com/rostagabor/mathmaze/MathMazeApplicationTests.kt
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 
-@SpringBootTest
+@SpringBootTest(properties = ["spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.HSQLDialect", "spring.datasource.url=jdbc:hsqldb:mem:23;DB_CLOSE_DELAY=-1;"])
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class MathMazeApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
