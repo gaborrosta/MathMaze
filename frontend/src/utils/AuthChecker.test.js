75c98d122499abe31d67103eea1112b9083a445e
Rosta Gábor <rostagabor13@gmail.com>
2024-03-29 19:08:58 +0000

    React: More tests

---
+++ frontend/src/utils/AuthChecker.test.js
@@ -0,0 +1,46 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { toBeInTheDocument } from "@testing-library/jest-dom";
+import { TokenContext } from "./TokenContext";
+import { MemoryRouter  } from "react-router-dom";
+import AuthChecker from "./AuthChecker";
+
+//Mock the Navigate component from react-router-dom
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  Navigate: (props) => {
+    return <div>{props.to} {props.replace ? "replace" : "" }</div>;
+  },
+}));
+
+//Mock a component that will be passed to the AuthChecker
+const MockComponent = () => <div>Mock Component</div>;
+
+//The test suite
+describe("AuthChecker", () => {
+  it("uses Navigate with replace when there is no token", async () => {
+    render(
+      <TokenContext.Provider value={{ token: null }}>
+        <MemoryRouter>
+          <AuthChecker Component={MockComponent} url="test-url" />
+        </MemoryRouter>
+      </TokenContext.Provider>
+    );
+
+    expect(screen.getByText("/login/?next=/test-url replace")).toBeInTheDocument();
+  });
+
+  it("renders the passed component when there is a token", () => {
+    render(
+      <TokenContext.Provider value={{ token: "test-token" }}>
+        <AuthChecker Component={MockComponent} url="test-url" />
+      </TokenContext.Provider>
+    );
+
+    expect(screen.getByText("Mock Component")).toBeInTheDocument();
+  });
+});
