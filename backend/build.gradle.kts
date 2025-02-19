7c505c8c83ae1c8007894676ad5cd0ec9b42edba
Rosta Gábor <rostagabor13@gmail.com>
2025-02-19 18:04:00 +0000

    Kotlin: Updated dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,11 +1,12 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-	id("org.springframework.boot") version "3.2.4"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.22"
-	kotlin("plugin.spring") version "1.9.23"
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm") version "2.1.10"
+	kotlin("plugin.spring") version "2.1.10"
 }
 
 group = "com.rostagabor"
@@ -25,10 +26,10 @@ repositories {
 
 dependencies {
 	//Spring Boot & Kotlin
-	implementation("org.springframework.boot:spring-boot-starter-web:3.2.4")
-	implementation("org.springframework.boot:spring-boot-starter-security:3.2.4")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
-	implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.23")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.4.2")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.4.2")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.10")
 
 	//ML
 	implementation("org.jetbrains.kotlinx:kotlin-deeplearning-tensorflow:0.5.2")
@@ -38,28 +39,28 @@ dependencies {
 	implementation(files("libs/opencv-490.jar"))
 
     //Database
-	implementation("org.hsqldb:hsqldb:2.7.2")
-	implementation("org.postgresql:postgresql:42.7.3")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
+	implementation("org.hsqldb:hsqldb:2.7.4")
+	implementation("org.postgresql:postgresql:42.7.5")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.2")
 
 	//Mail
-	implementation("org.springframework.boot:spring-boot-starter-mail:3.2.4")
+	implementation("org.springframework.boot:spring-boot-starter-mail:3.4.2")
 
 	//JWT
-	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-	implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
-	implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
 	//Tests
-	testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.4") { exclude(module = "mockito-core") }
-	testImplementation("io.mockk:mockk:1.13.10")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.2") { exclude(module = "mockito-core") }
+	testImplementation("io.mockk:mockk:1.13.16")
 	testImplementation("com.ninja-squad:springmockk:4.0.2")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
+	compilerOptions {
+		freeCompilerArgs.add("-Xjsr305=strict")
+		jvmTarget.set(JvmTarget.JVM_17)
 	}
 }
 
