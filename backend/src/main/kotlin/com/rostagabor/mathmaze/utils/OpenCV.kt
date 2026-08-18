eb95e5afd65af63c85b8496eb69700218e874b32
Rosta Gábor <rostagabor13@gmail.com>
2026-08-18 20:28:17 +0100

    Kotlin: Changed OpenCV

--- backend/src/main/kotlin/com/rostagabor/mathmaze/utils/OpenCV.kt
+++ backend/src/main/kotlin/com/rostagabor/mathmaze/utils/OpenCV.kt
@@ -1,7 +1,9 @@
 package com.rostagabor.mathmaze.utils
 
 import org.bytedeco.javacpp.Loader
-import org.bytedeco.opencv.opencv_java
+import org.bytedeco.opencv.global.opencv_core
+import org.bytedeco.opencv.global.opencv_imgcodecs
+import org.bytedeco.opencv.global.opencv_imgproc
 import org.opencv.core.Core
 
 /**
@@ -9,7 +11,9 @@ import org.opencv.core.Core
  */
 @Throws(Exception::class)
 fun loadOpenCV() {
-    Loader.load(opencv_java::class.java)
+    Loader.load(opencv_core::class.java)
+    Loader.load(opencv_imgproc::class.java)
+    Loader.load(opencv_imgcodecs::class.java)
 
     //Check if OpenCV is loaded (it will throw an exception if it is not)
     println("OpenCV version: ${Core.VERSION}")
